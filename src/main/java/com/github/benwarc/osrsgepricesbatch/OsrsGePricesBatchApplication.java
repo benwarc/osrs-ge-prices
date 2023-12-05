51baa61222e7bf3d2f32bec9c7f12f2f62a71788
benwarc <benwarc9@gmail.com>
2023-12-04 22:03:08 -0500

    Added empty Spring Batch project

---
+++ src/main/java/com/github/benwarc/osrsgepricesbatch/OsrsGePricesBatchApplication.java
@@ -0,0 +1,13 @@
+package com.github.benwarc.osrsgepricesbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsrsGePricesBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OsrsGePricesBatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/benwarc/osrsgepricesbatch/OsrsGePricesBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.benwarc.osrsgepricesbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OsrsGePricesBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
