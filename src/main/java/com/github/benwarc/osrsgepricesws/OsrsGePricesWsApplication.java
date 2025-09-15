d5ce023e2a13c5bb53450b91195250c4e55e815f
benwarc <benwarc9@gmail.com>
2025-09-15 07:59:04 -0400

    Added empty Spring Web project

---
+++ src/main/java/com/github/benwarc/osrsgepricesws/OsrsGePricesWsApplication.java
@@ -0,0 +1,13 @@
+package com.github.benwarc.osrsgepricesws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsrsGePricesWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OsrsGePricesWsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/benwarc/osrsgepricesws/OsrsGePricesWsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.benwarc.osrsgepricesws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OsrsGePricesWsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
